b093e1967ce0b57d6d0c507ecb09d2a948e66587
Alessio Persichetti <alessio.persichetti@ectmingegneria.it>
2024-03-26 13:25:42 +0100

    small fix

--- internal/domain/models/case.go
+++ internal/domain/models/case.go
@@ -13,7 +13,6 @@ type Case struct {
 	Messages        []Message            `json:"messages" bson:"messages"`
 	CollaboratorIDs []primitive.ObjectID `json:"collaborator_ids" bson:"collaborator_ids"`
 	Action          string               `json:"action" bson:"action"`
-	Skill           string               `json:"skill" bson:"skill"`
 	AgentID         primitive.ObjectID   `json:"agent_id" bson:"agent_id"`
 	LastEdit        time.Time            `json:"last_edit" bson:"last_edit"`
 	Share           bool                 `json:"share" bson:"share"`
@@ -24,4 +23,5 @@ type Message struct {
 	Content     string             `json:"content" bson:"content"`
 	SenderID    primitive.ObjectID `json:"sender_id" bson:"sender_id"`
 	RecipientID primitive.ObjectID `json:"recipient_id" bson:"recipient_id"`
+	Skill       string             `json:"skill" bson:"skill"`
 }
