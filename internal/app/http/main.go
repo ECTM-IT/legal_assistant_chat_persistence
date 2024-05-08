6fcd52f344b1498496dfc1ef851db5825837071c
devops.consulting <devops.consulting@ongraph.com>
2024-05-08 14:36:20 +0530

    removed logger

--- internal/app/http/main.go
+++ internal/app/http/main.go
@@ -28,14 +28,12 @@ func Main() {
 	}
 }
 
-func loadConfig() Config { 
-	logger := logs.Init()
+func loadConfig() Config {
 	var cfg Config 
-	cfg.BaseURL = env.GetString("BASE_URL", "http://0.0.0.0:4444") 
-	cfg.HTTPPort = env.GetInt("HTTP_PORT", 4444) 
-	cfg.Cookie.SecretKey = env.GetString("COOKIE_SECRET_KEY", "3iepwbkq5chsrusjoha26mnsjt233ujq") 
+	cfg.BaseURL = env.GetString("BASE_URL", "http://0.0.0.0:4444")
+	cfg.HTTPPort = env.GetInt("HTTP_PORT", 4444)
+	cfg.Cookie.SecretKey = env.GetString("COOKIE_SECRET_KEY", "3iepwbkq5chsrusjoha26mnsjt233ujq")
 	cfg.MongoDB.URI = env.GetString("MONGODB_URI", "mongodb+srv://alessiopersichetti:r9BtY7WjGv6ck5OS@latest.smobjvj.mongodb.net/?retryWrites=true&w=majority&appName=latest")
 	cfg.MongoDB.Database = env.GetString("MONGODB_DATABASE", "legal_assistant")
-	logger.Info(cfg.MongoDB.URI)
 	return cfg 
 }
