3b5e929354ff8d15e1b373b78578dd380f1f4a01
abekenov-ecr <abekenov@ecr.ai>
2024-11-04 12:57:45 +0600

    Remove unnecessary changes

--- internal/app/http/main.go
+++ internal/app/http/main.go
@@ -31,8 +31,7 @@ func loadConfig(logger logs.Logger) Config {
 	cfg.BaseURL = env.GetString("BASE_URL", "http://0.0.0.0:4444")
 	cfg.HTTPPort = env.GetInt("HTTP_PORT", 4444)
 	cfg.Cookie.SecretKey = env.GetString("COOKIE_SECRET_KEY", "3iepwbkq5chsrusjoha26mnsjt233ujq")
-	// cfg.MongoDB.URI = env.GetString("MONGODB_URI", "mongodb+srv://alessiopersichetti:r9BtY7WjGv6ck5OS@latest.smobjvj.mongodb.net/?retryWrites=true&w=majority&appName=latest")
-	cfg.MongoDB.URI = env.GetString("MONGODB_URI", "mongodb://localhost:27017")
+	cfg.MongoDB.URI = env.GetString("MONGODB_URI", "mongodb+srv://alessiopersichetti:r9BtY7WjGv6ck5OS@latest.smobjvj.mongodb.net/?retryWrites=true&w=majority&appName=latest")
 	cfg.MongoDB.Database = env.GetString("MONGODB_DATABASE", "legal_assistant")
 
 	logger.Info("Configuration loaded",
