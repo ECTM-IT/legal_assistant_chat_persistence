57d62357252340d49497834ea7043a1457ba4d89
Alessio Persichetti <alessio.persichetti@ectmingegneria.it>
2024-11-06 14:44:21 +0100

    bug fix

--- internal/app/pkg/security/env_key_manager.go
+++ internal/app/pkg/security/env_key_manager.go
@@ -1,7 +1,9 @@
 package security
 
 import (
+	"encoding/base64"
 	"errors"
+	"fmt"
 	"os"
 )
 
@@ -19,11 +21,16 @@ func NewEnvironmentKeyManager(envVar string) *EnvironmentKeyManager {
 // GetKey retrieves the AES-256 key from the specified environment variable.
 func (e *EnvironmentKeyManager) GetKey() ([]byte, error) {
 	key := os.Getenv(e.envVar)
+	decodedKey, err := base64.StdEncoding.DecodeString(key)
 	if key == "" {
 		return nil, ErrKeyNotFound
 	}
-	if len(key) != 32 {
-		return nil, errors.New("AES-256 key must be 32 bytes long")
+	if err != nil {
+		fmt.Println("error decoding Base64 key:", err)
+		return nil, errors.New("error decoding Base64 key")
 	}
-	return []byte(key), nil
+	if len(decodedKey) == 32 {
+		return decodedKey, nil
+	}
+	return nil, errors.New("AES-256 key must be 32 bytes long")
 }
