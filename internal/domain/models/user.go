12cf382fe6e49a959b62be91a61f41cc49d7cbfc
Alessio Persichetti <alessio.persichetti@ectmingegneria.it>
2024-11-13 12:45:28 +0100

    Revert "sprint 4 features completed"
    This reverts commit 1dc85ec4a78a980efdf58f505866483be78abc01.

--- internal/domain/models/user.go
+++ internal/domain/models/user.go
@@ -1,25 +1,18 @@
 package models
 
 import (
-	"time"
-
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
 	ID             primitive.ObjectID   `json:"id" bson:"_id,omitempty"`
-	EncryptedName  string               `json:"encrypted_name" bson:"encrypted_name"`
-	EncryptedEmail string               `json:"encrypted_email" bson:"encrypted_email"`
 	Image          string               `json:"image" bson:"image"`
+	Email          string               `json:"email" bson:"email"`
 	FirstName      string               `json:"first_name" bson:"first_name"`
 	LastName       string               `json:"last_name" bson:"last_name"`
 	Phone          string               `json:"phone" bson:"phone"`
 	CaseIDs        []primitive.ObjectID `json:"case_ids" bson:"case_ids"`
 	TeamID         primitive.ObjectID   `json:"team_id" bson:"team_id"`
 	AgentIDs       []primitive.ObjectID `json:"agent_ids" bson:"agent_ids"`
-	SubscriptionID primitive.ObjectID   `json:"subscription_id" bson:"subscription_id"`
-	CreationDate   time.Time            `json:"creation_date" bson:"creation_date"`
-	LastEdit       time.Time            `json:"last_edit" bson:"last_edit"`
-	Share          bool                 `json:"share" bson:"share"`
-	IsArchived     bool                 `json:"is_archived" bson:"is_archived"`
+	SubscriptionID primitive.ObjectID   `json:"subscription_id" bson:"subscription_id"` //choose encoding strategy to handle payments
 }
