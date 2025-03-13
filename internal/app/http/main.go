ceb634380c926d47a717c7c7c5d39725ba43d96a
dinohurem <dino_hurem@hotmail.com>
2025-03-13 16:17:30 +0100

    UPDATE | Change MongoDB URI and add server run instruction to README

--- internal/app/http/main.go
+++ internal/app/http/main.go
@@ -31,7 +31,7 @@ func loadConfig(logger logs.Logger) Config {
 	cfg.BaseURL = env.GetString("BASE_URL", "http://0.0.0.0:4444")
 	cfg.HTTPPort = env.GetInt("HTTP_PORT", 4444)
 	cfg.Cookie.SecretKey = env.GetString("COOKIE_SECRET_KEY", "3iepwbkq5chsrusjoha26mnsjt233ujq")
-	cfg.MongoDB.URI = env.GetString("MONGODB_URI", "mongodb+srv://alessiopersichetti:r9BtY7WjGv6ck5OS@latest.smobjvj.mongodb.net/?retryWrites=true&w=majority&appName=latest")
+	cfg.MongoDB.URI = env.GetString("MONGODB_URI", "mongodb://legal-assist-chat-persistence-dev-qa:AFWmqIy38KoY8pquvfuZmNFmEdUoYJ139dQAYmNhS7urHjVjOVDliCgNMfQhCOED6m1DJ3uxi8k3ACDbvUuSog%3D%3D@legal-assist-chat-persistence-dev-qa.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000")
 	cfg.MongoDB.Database = env.GetString("MONGODB_DATABASE", "legal_assistant")
 
 	logger.Info("Configuration loaded",
